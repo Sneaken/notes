9eba4592dc1c6a0688a6104789eb737da188fae6
Sneanken <924393527@qq.com>
2020-07-16 11:53:42 +0800

    凑硬币 用dp table 实现

--- 算法/套路/code/memo.js
+++ 算法/套路/code/memo.js
@@ -27,18 +27,33 @@
 //   return memo[n];
 // }
 
-function fib (N) {
-  if (N === 2 || N === 1) return 1
-  let prev = 1,
-      curr = 1
-  for (let i = 3; i <= N; i++) {
-    let sum = prev + curr
-    prev    = curr
-    curr    = sum
-  }
-  return curr
+// function fib(N) {
+//   if (N === 2 || N === 1) return 1;
+//   let prev = 1,
+//     curr = 1;
+//   for (let i = 3; i <= N; i++) {
+//     let sum = prev + curr;
+//     prev = curr;
+//     curr = sum;
+//   }
+//   return curr;
+// }
+
+// console.time("带缓存时间");
+// console.log(fib(22222));
+// console.timeEnd("带缓存时间");
+
+// 凑硬币问题
+function coinChange (coins, amount) {
+  const dp = Array(amount + 1).fill(amount + 1)
+  dp[0]    = 0
+  dp.forEach((item, index) => {
+    coins.forEach(coin => {
+      if (index - coin < 0) return
+      dp[index] = Math.min(dp[index], 1 + dp[index - coin])
+    })
+  })
+  return dp[amount] === amount + 1 ? -1 : dp[amount]
 }
 
-console.time("带缓存时间")
-console.log(fib(22222))
-console.timeEnd("带缓存时间")
+console.log(coinChange([1, 2, 5], 11))
